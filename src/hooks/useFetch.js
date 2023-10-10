c39f7538d33bf12f343d8b8352389a821ec75b56
cristobalquilimaco <quilimacox1@gmail.com>
2023-10-10 06:51:12 -0500

    Se crea variable update register para manejar la actualizacion de los usuarios en usefetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,49 @@
+import axios from "axios"
+import { useState } from "react"
+
+
+const useFetch = (baseUrl) => {
+    const [infoApi, setInfoApi] = useState()
+
+    //READ
+    const getApi = (path) =>{
+        const url = `${baseUrl}${path}/`
+        axios.get(url)
+        .then(res => setInfoApi(res.data))
+        .catch(err => console.log(err))
+    }
+
+    //CREATE
+
+    const createNewRegister = (path, data) =>{
+        const url = `${baseUrl}${path}/`
+        axios.post(url, data)
+        .then(res => {
+            console.log(res.data)
+            setInfoApi([...infoApi, res.data])
+        })
+        .catch(err => console.log(err))
+    }
+
+    //DELETE
+
+    const deleteRegister = (path, id) =>{
+        const url = `${baseUrl}${path}/${id}/`
+        axios.delete(url)
+        .then(res => {
+            console.log(res.data)
+            const infoApiFilter = infoApi.filter(element => element.id !== id)
+            setInfoApi(infoApiFilter)
+        })
+        .catch(err => console.log(err))
+    }
+
+    //UPDATE
+
+    const updateRegister = (path, id) =>{
+        const url = `${baseUrl}${path}/${id}/`
+    }
+
+    return[infoApi, getApi, createNewRegister, deleteRegister]
+
+}

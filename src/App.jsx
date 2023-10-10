b362d619e3fb6c817d50911c236742a58da2a260
cristobalquilimaco <quilimacox1@gmail.com>
2023-10-10 07:14:11 -0500

    Se importa hook usefetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import useFetch from "./hooks/useFetch"
 
 function App() {
+  const baseUrl = "https://users-crud.academlo.tech/"
+  const [
+    users, 
+    getAllUsers, 
+    createNewUser, 
+    deleteUserById,
+    updateUserById
+  ] = useFetch(baseUrl)
 
   return (
 <div>
